57d3ec70d2c0e5ba08d3e1058c0994eeaccd77c0
JASavage <javs0701@gmail.com>
2017-04-04 08:24:25 -0400

    Update RPG_Func_Alaa.py

--- RPG_Func_Alaa.py
+++ RPG_Func_Alaa.py
@@ -36,6 +36,39 @@ class rarity(Item):
     damage_add = item.rarity
     defense_add = item.rarity
     
-  
+  def level_up:
+    add_damage_chance = 4
+    add_health_chance = 6
+    add_regen_chance = 2
+    add_defense_chance = 8
+    add_test_1 = randint(1,10)
+    add_test_2 = randint(1,10)
+    add_test_3 = randint(1,10)
+    add_test_4 = randint(1,10)
+    Player.__init__
+    lvl_damage_add = int(.05(player.damage))
+    lvl_defense_add = int(.15(player.defense))
+    lvl_regen_add = float(.1)
+    lvl_health_add = int(.1(player.health))
+    if lvl_defense_add < 1:
+      lvl_defense_add = 1
+    if lvl_damage_add < 1:
+      lvl_damage_add = 1
+    if add_test_1 <= add_damage_chance:
+      player.damage += lvl_damage_add
+    else:
+        player.damage = player.damage
+    if add_test_2 >= add_defense:
+      player.defense += lvl_damage_add
+    else:
+      player.defense = player.defense
+    if add_test_3 <= add_regen_chance:
+      player.health_regen += lvl_regen_add
+    else:
+      player.health_regen = player.health_regen
+    if add_test_4 >= add_health_chance:
+      player.health += lvl_health_add
+    else:
+      player.health = player.health
   
     
